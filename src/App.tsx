67a892190a5bc4ef7852ace8f3fe617ab4673988
wzc <newwangzhicheng@hotmail.com>
2021-06-10 17:34:41 +0800

    添加antdv

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ export default defineComponent({
   name: 'App',
   setup() {
     return () => (
-      <img src={logoUrl} alt="Vue logo" />
+      <>
+        <img src={logoUrl} alt="Vue logo" />
+        <a-button type="primary">Antd按钮</a-button>
+      </>
     )
   }
 })
